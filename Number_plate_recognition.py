a96cfb8478d533e8311066c4326e835d4d4b5459
sawarn69 <sawarn69@gmail.com>
2018-08-28 22:20:47 +0530

    Making the image ready

--- Number_plate_recognition.py
+++ Number_plate_recognition.py
@@ -4,3 +4,15 @@ from copy import deepcopy
 from PIL import Image
 import pytesseract as tess
 import os
+def preprocess(img):
+	# cv2.imshow("Input",img)
+	imgBlurred = cv2.GaussianBlur(img, (5,5), 0)
+	gray = cv2.cvtColor(imgBlurred, cv2.COLOR_BGR2GRAY)
+
+	sobelx = cv2.Sobel(gray,cv2.CV_8U,1,0,ksize=3)
+	cv2.imshow("Sobel",sobelx)
+	cv2.waitKey(0)
+	ret2,threshold_img = cv2.threshold(sobelx,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+	cv2.imshow("Threshold",threshold_img)
+	cv2.waitKey(0)
+	return threshold_img
