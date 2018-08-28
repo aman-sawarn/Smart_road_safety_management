45b37602ba24494678dabfa7494bf127fb1c6cbc
sawarn69 <sawarn69@gmail.com>
2018-08-28 22:21:55 +0530

    cleaning the number plate after detecting

--- Number_plate_recognition.py
+++ Number_plate_recognition.py
@@ -16,3 +16,29 @@ def preprocess(img):
 	cv2.imshow("Threshold",threshold_img)
 	cv2.waitKey(0)
 	return threshold_img
+def cleanPlate(plate):
+	print "CLEANING PLATE. . ."
+	gray = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
+	#kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+	#thresh= cv2.dilate(gray, kernel, iterations=1)
+
+	_, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+	im1,contours,hierarchy = cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+	if contours:
+		areas = [cv2.contourArea(c) for c in contours]
+		max_index = np.argmax(areas)
+
+		max_cnt = contours[max_index]
+		max_cntArea = areas[max_index]
+		x,y,w,h = cv2.boundingRect(max_cnt)
+
+		if not ratioCheck(max_cntArea,w,h):
+			return plate,None
+
+		cleaned_final = thresh[y:y+h, x:x+w]
+		#cv2.imshow("Function Test",cleaned_final)
+		return cleaned_final,[x,y,w,h]
+
+	else:
+		return plate,None
